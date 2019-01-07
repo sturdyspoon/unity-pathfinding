26f8680713de52fe843cf7f1e4a16af88cdcbcf2
Anton Pantev <antonpantev123@gmail.com>
2019-01-07 08:10:29 -0500

    Start of project. Creating a random tilemap.

---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelBuilder : MonoBehaviour
+{
+    public int width = 100;
+    public int height = 100;
+    public float perlinScale = 10f;
+
+    public Tile ground;
+    public Tile wall;
+
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+
+        PerlinNoiseGrid noise = new PerlinNoiseGrid(width, height, perlinScale);
+
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Tile t = (noise[i, j] < 0.5) ? ground : wall;
+                tilemap.SetTile(new Vector3Int(i, j, 0), t);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PerlinNoiseGrid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PerlinNoiseGrid
+{
+    public int width;
+    public int height;
+    public Vector2 offset;
+    public Vector2 scale;
+
+    public float this[int x, int y]
+    {
+        get
+        {
+            float i = offset.x + (float)x / width * scale.x;
+            float j = offset.y + (float)y / height * scale.y;
+
+            return Mathf.PerlinNoise(i, j);
+        }
+    }
+
+    public PerlinNoiseGrid(int width, int height, float perlinScale)
+    {
+        this.width = width;
+        this.height = height;
+
+        offset = new Vector2(Random.value * 100000, Random.value * 100000);
+
+        float rAspect = (float)width / height;
+        scale = new Vector2(perlinScale * rAspect, perlinScale);
+    }
+}
